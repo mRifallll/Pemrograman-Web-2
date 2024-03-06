843e30cca35d98c89a5b135efdab46787dda9a46
mRifallll <96682830+mRifallll@users.noreply.github.com>
2024-03-06 21:58:05 +0700

    pertemuan pert 1 web 2

---
+++ Pertemuan1/Pertemuan1.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Pertemuan 1</title>
+</head>
+<body>
+
+    <?php
+        print"<pre>";
+            print"<center><font size= 18 color='red'>BIODATA</center></font><br>";
+            print"<center><font size= 12 color ='blue'> Nama :<b>Muhammad Rifal Junaidi</b></center></font>";
+            print"<center><font size= 12 color ='blue'> Email :m.rifaljunaidi83@gmail.com </center></font>";
+            print"<center><font size= 12 color ='blue'> Jurusan :Teknik Informatika</center></font>";
+            print"<center><font size= 12 color ='blue'> Hoby :Programing</center></font></pre>";
+    ?>
+</body>
+</html>
---
+++ Pertemuan1/Pertemuan_1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pertemuan 1</title>
+    <style>
+        .container {
+            font-family : Arial;
+            font-size : 10px;
+            color : blue;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <?php
+            $text = "Saya baru menggunakan bahasa pemrograman php ini dan menurut saya ini adalah suatu hal yang menyenangkan sekaligus memusingkan karena saya baru belajar bahasa php ini, tapi bahasa php ini harus terus saya pelajari dan kembangkan terus sampai nantinya saya bisa menjadi seorang backend yang menggunakan bahasa pemrograman php";
+            echo "<h1>$text</h1>";
+        ?>
+    </div>
+</body>
+</html>
